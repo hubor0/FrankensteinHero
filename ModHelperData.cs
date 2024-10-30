f761311bc520922b957e0fc1b58e9a8e225ad329
hubor <minecraftcostam2009@gmail.com>
2024-10-30 07:56:28 +0100

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -5,7 +5,7 @@ public static class ModHelperData
     public const string WorksOnVersion = "45.0";
     public const string Version = "1.0.0";
     public const string Name = "Frankenstein Hero";
-    public const string Description = "Adds a new custom Frankenstein themed Hero. complete with all 20 upgrades, art and 3d models! NOTE: The game tends to lag Alot when the tower has too many buffs.";
+    public const string Description = "Adds a new custom Frankenstein themed Hero. complete with all 20 upgrades, art and 3d models! NOTE: The game tends to lag Alot when the tower has too many buffs.RECOMMENDED TO USE ON MELONLOADER VERSION 0.6.5";
 
     public const string RepoOwner = "hubor0"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "Frankenstein-Hero"; // TODO add your repo name here, also in the download url in README.md
